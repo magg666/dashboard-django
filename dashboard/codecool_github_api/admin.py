74fa8086dc7126db948b4886c5f9b15d38ab81af
magg666 <mw23127@gmail.com>
2019-08-26 20:34:21 +0200

    registered all models

--- dashboard/codecool_github_api/admin.py
+++ dashboard/codecool_github_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Repository, WeeklyStatistic, TotalStatistic
 
-# Register your models here.
+admin.register(Repository)
+admin.register(WeeklyStatistic)
+admin.register(TotalStatistic)
